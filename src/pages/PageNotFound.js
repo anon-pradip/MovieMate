533b77118dff4cc64fd83060e3cbff9b4622259b
anon-pradip <pradipsubedi360@gmail.com>
2023-04-06 17:11:29 +0545

    make page not found

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import notFound from '../assets/404.jpg'
 const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
+    <div className=' flex justify-center items-center flex-col space-y-2 lg:space-y-7 pt-5 px-2'>
+      <p className=' dark:text-white text-2xl py-7 lg:py-4 md:text-6xl capitalize text-center'>oops! Page not found</p>
+      <div className=' max-w-lg'>
+        <img src={notFound} alt="" className='rounded-t-md' />
+      </div>
+    </div>
   )
 }
 
